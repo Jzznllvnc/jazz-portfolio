787263b6cc8a01c591ec1dd8d613bdf88b1c1c4c
Jazz <jazznelle002@gmail.com>
2025-07-28 02:43:38 +0800

    deployment

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import Script from 'next/script';
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -29,7 +30,9 @@ export default function RootLayout({ children }) {
           }
         `}</style>
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+        <Analytics />
+      </body>
     </html>
   );
 }
